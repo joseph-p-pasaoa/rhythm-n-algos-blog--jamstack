54f42ad9d73194429c13ea3cb5dd216dbf31af4f
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-07-21 23:58:02 -0400

    add portfolio home link to top nav

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -19,7 +19,10 @@ const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
       <nav className={cn(styles.nav, showNav && styles.showNav)}>
         <ul>
           <li>
-            <Link to='/archive/'>Archive</Link>
+            <Link to='/archive/'>Archived Posts</Link>
+          </li>|
+          <li>
+            <a href="https://dev.josephpasaoa.com">Portfolio Home</a>
           </li>
         </ul>
       </nav>
